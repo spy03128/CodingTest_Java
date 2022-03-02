245f846397662196197a1c5703c21ec458c382cf
jihyeon <spy03128@naver.com>
2022-03-02 18:26:40 +0900

    BJ 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/BAEKJOON/B5_18108.java
@@ -0,0 +1,13 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class B5_18108 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		System.out.println(x-543);
+	}
+}
