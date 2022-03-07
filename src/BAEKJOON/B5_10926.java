e8df463f8c255c3e7df6edcf8d7620b9c07876b4
jihyeon <spy03128@naver.com>
2022-03-07 13:53:13 +0900

    BJ 10926 ??!

---
+++ src/BAEKJOON/B5_10926.java
@@ -0,0 +1,13 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class B5_10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = sc.nextLine();
+		
+		System.out.println(s+"??!");
+	}
+}
