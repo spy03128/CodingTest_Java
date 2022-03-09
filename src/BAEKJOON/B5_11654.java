0df28d7eb77688f3a17e1efbb241b522c3e12d8b
jihyeon <spy03128@naver.com>
2022-03-09 22:56:27 +0900

    BJ 11654 아스키 코드

---
+++ src/BAEKJOON/B5_11654.java
@@ -0,0 +1,12 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class B5_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ch = sc.next();
+		
+		System.out.print((int)ch.charAt(0));
+	}
+}
