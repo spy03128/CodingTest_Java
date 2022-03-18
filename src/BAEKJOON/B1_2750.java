fd4bedc59df1a0926193007bc5eabe433061b343
jihyeon <spy03128@naver.com>
2022-03-18 22:35:28 +0900

    BJ 2750 수 정렬하기

---
+++ src/BAEKJOON/B1_2750.java
@@ -0,0 +1,23 @@
+package BAEKJOON;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B1_2750 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int arr[] = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i=0;i<N;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
