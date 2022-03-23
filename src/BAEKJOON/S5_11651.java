491171037acec2805e2f183329138e9409fc53e0
jihyeon <spy03128@naver.com>
2022-03-23 23:43:01 +0900

    BJ 11651 좌표 정렬하기 2

---
+++ src/BAEKJOON/S5_11651.java
@@ -0,0 +1,49 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class S5_11651 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		Integer arr[][] = new Integer[N][2];
+		
+		StringTokenizer st; 
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+			
+		}
+		
+		Arrays.sort(arr,new Comparator<Integer[]>(){
+
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				// TODO Auto-generated method stub
+			
+				if((int)o1[1]==(int)o2[1]) {
+					
+					return o1[0]-o2[0];
+				}
+				
+				return o1[1]-o2[1];
+			}
+
+		
+			
+		});
+		
+		for(int i=0;i<N;i++) {
+			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
