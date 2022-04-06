3ad57cc18d6ffe056aab601e4d4dec287251d1e9
jihyeon <spy03128@naver.com>
2022-04-06 16:37:46 +0900

    BJ 2458 키 순서

--- src/BAEKJOON/G4_2458.java
+++ src/BAEKJOON/G4_2458.java
@@ -3,7 +3,6 @@ package BAEKJOON;
 import java.util.Scanner;
 
 public class G4_2458 {
-	
 
 	static final int INF = 9999999;
 	static int N, M, adjMatrix[][];
@@ -11,90 +10,67 @@ public class G4_2458 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-		int T=sc.nextInt();
 
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			
-			
-			N = sc.nextInt(); //학생 수 
-			M = sc.nextInt(); //키 비교횟수
-			adjMatrix = new int[N][N];
-			for (int i = 0; i < N; ++i) {
-				for (int j = 0; j < N; ++j) {
-						adjMatrix[i][j] = INF;		
-				}
-			}
-			
-			
-			
-			for(int i=0;i<M;i++) {
-				int x =sc.nextInt();
-				int y =sc.nextInt();
-				
-				adjMatrix[x-1][y-1] = 1;		
-			}
-			
-			
-			for(int i=0;i<N;i++) {
-
-				adjMatrix[i][i] = 0;		
-			}
-			
-			
-			
-
-			for (int k = 0; k < N; ++k) {
-				for (int i = 0; i < N; ++i) {
-					if (i == k)
-						continue; 
-					for (int j = 0; j < N; ++j) {
-						if (i == j || k == j)
-							continue;
-						if (adjMatrix[i][j] > adjMatrix[i][k] + adjMatrix[k][j]) {
-							adjMatrix[i][j] = adjMatrix[i][k] + adjMatrix[k][j];
-						}
-					}
-				}
-		
-			}
-			
-
-			
-			int cnt = 0;
-			
+		N = sc.nextInt(); // 학생 수
+		M = sc.nextInt(); // 키 비교횟수
+		adjMatrix = new int[N][N];
+		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < N; ++j) {
-				boolean notnum = false;
-				for (int i = 0; i < N; ++i) {
-					
-					if(adjMatrix[i][j]==INF) {
-						
-						if(adjMatrix[j][i]==INF) {
-							notnum = true;
-							break;
-						}
-						
-					}
-					
-				}
-				
-				if(notnum==false) cnt++;
-				
-				
+				adjMatrix[i][j] = INF;
 			}
-			
-			System.out.println("#"+ test_case+" "+cnt);
-			
-			
 		}
-		
-		
-		
+
+		for (int i = 0; i < M; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			adjMatrix[x - 1][y - 1] = 1;
+		}
+
+		for (int i = 0; i < N; i++) {
+
+			adjMatrix[i][i] = 0;
+		}
+
+		for (int k = 0; k < N; ++k) {
+			for (int i = 0; i < N; ++i) {
+				if (i == k)
+					continue;
+				for (int j = 0; j < N; ++j) {
+					if (i == j || k == j)
+						continue;
+					if (adjMatrix[i][j] > adjMatrix[i][k] + adjMatrix[k][j]) {
+						adjMatrix[i][j] = adjMatrix[i][k] + adjMatrix[k][j];
+					}
+				}
+			}
+
+		}
+
+		int cnt = 0;
+
+		for (int j = 0; j < N; ++j) {
+			boolean notnum = false;
+			for (int i = 0; i < N; ++i) {
+
+				if (adjMatrix[i][j] == INF) {
+
+					if (adjMatrix[j][i] == INF) {
+						notnum = true;
+						break;
+					}
+
+				}
+
+			}
+
+			if (notnum == false)
+				cnt++;
+
+		}
+
+		System.out.println(cnt);
 
 	}
-
-
 
 }
