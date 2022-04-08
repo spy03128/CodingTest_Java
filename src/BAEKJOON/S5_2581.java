d5721725c0726f09b70ec216410119211fc7bf48
jihyeon <spy03128@naver.com>
2022-04-09 08:27:36 +0900

    BJ 2581 소수

---
+++ src/BAEKJOON/S5_2581.java
@@ -0,0 +1,48 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class S5_2581 {
+public static boolean prime[];
+	
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		prime = new boolean[N + 1];	
+
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+			for(int j = i * i; j < prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
+		
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i = M; i <= N; i++) {
+			if(prime[i] == false) {	
+				sum += i;
+				if(min == Integer.MAX_VALUE) {	
+					min = i;
+				}
+			}
+		}
+		
+		if(sum == 0) {
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+		
+	}
+ 
+
+}
