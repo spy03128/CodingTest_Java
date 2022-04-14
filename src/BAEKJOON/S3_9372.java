a4785b36a26d389dffb19ff0864dbec7245c5c36
jihyeon <spy03128@naver.com>
2022-04-14 19:51:39 +0900

    BJ 9372 상근이의 여행

---
+++ src/BAEKJOON/S3_9372.java
@@ -0,0 +1,24 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class S3_9372 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int tc = sc.nextInt();
+		
+		for(int i=0;i<tc;i++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+			
+			
+			for(int j=0;j<m;j++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+			}
+			
+			System.out.println(n-1);
+		}
+	}
+}
