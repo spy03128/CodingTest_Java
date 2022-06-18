3992d4dad7849c54c945cd8978286f8fcf6b181d
jihyeon <spy03128@naver.com>
2022-06-18 17:56:18 +0900

    BJ 11719 그대로 출력하기 2

---
+++ src/BAEKJOON/B2_11719.java
@@ -0,0 +1,18 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class B2_11719 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		while(sc.hasNext()) {
+			
+			String s = sc.nextLine();
+			
+			System.out.println(s);
+		}
+	}
+	
+}
