2ce1d947c75dc620e0f20a1a4988835fd024a26d
jihyeon <spy03128@naver.com>
2022-06-25 15:59:13 +0900

    BJ 1264 모음의 개수

--- src/BAEKJOON/B4_1264.java
+++ src/BAEKJOON/B4_1264.java
@@ -1,6 +1,7 @@
 package BAEKJOON;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class B4_1264 {
@@ -12,14 +13,24 @@ public class B4_1264 {
 			
 			if(s.equals("#")) break;
 			
-			String pattern = "(?i)[aeiou]";
+//			String pattern = "(?i)[aeiou]";
 			int count=0;
-			for(int i=0;i<s.length();i++) {
-				String temp = s.charAt(i)+"";
-				if(Pattern.matches(pattern,temp)) count++;
+//			for(int i=0;i<s.length();i++) {
+//				String temp = s.charAt(i)+"";
+//				if(Pattern.matches(pattern,s)) count++;
+//			}
+//			
+
+			
+			Pattern pattern = Pattern.compile("(?i)[aeiou]");
+			Matcher matcher = pattern.matcher(s);
+			
+			while(matcher.find()) {
+				count++;
 			}
 			
 			System.out.println(count);
+			
 			
 		}
 	}
