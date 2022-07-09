6e4760338014fdcd6bcb61233c947e17b5da016a
jihyeon <spy03128@naver.com>
2022-07-09 18:59:53 +0900

    BJ 2338 긴자리 계산

---
+++ src/BAEKJOON/B5_2338.java
@@ -0,0 +1,18 @@
+package BAEKJOON;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B5_2338 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+
+		System.out.println(a.add(b));
+		System.out.println(a.subtract(b));
+		System.out.println(a.multiply(b));
+
+	}
+}
