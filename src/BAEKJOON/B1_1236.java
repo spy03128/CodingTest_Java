adcda42a64372c5c01b7dc09cfcf1f5a08a5df4b
jihyeon <spy03128@naver.com>
2022-08-23 00:53:35 +0900

    BJ 1236 성 지키기

--- src/BAEKJOON/B1_1236.java
+++ src/BAEKJOON/B1_1236.java
@@ -3,7 +3,6 @@ package BAEKJOON;
 import java.util.Scanner;
 
 public class B1_1236 {
-
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();	
