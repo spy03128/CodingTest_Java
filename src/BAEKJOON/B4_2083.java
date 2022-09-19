dd19a59bcf98f18b377238a6e286df2baf486ecc
jihyeon <spy03128@naver.com>
2022-09-19 23:57:17 +0900

    BJ 2083 럭비 클럽

---
+++ src/BAEKJOON/B4_2083.java
@@ -0,0 +1,24 @@
+package BAEKJOON;
+import java.util.Scanner;
+
+public class B4_2083 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.next();
+            int age = sc.nextInt();
+            int weight = sc.nextInt();
+
+            if (str.equals("#") && age == 0 && weight ==0) {
+                break;
+            }
+
+            if (age > 17 || weight >= 80) {
+                System.out.println(str +" "+"Senior");
+            } else  {
+                System.out.println(str +" "+"Junior");
+            }
+        }
+    }
+}
