d7c5592a962cad0a6ed52c48fc2d7fc18d97c75a
jihyeon <spy03128@naver.com>
2022-09-25 22:39:12 +0900

    BJ 2033 반올림

---
+++ src/BAEKJOON/B1_2033.java
@@ -0,0 +1,24 @@
+package BAEKJOON;
+import java.util.Scanner;
+
+public class B1_2033 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char[] chArr = str.toCharArray();
+		int len = chArr.length;
+ 
+		int nextAdd = 0;
+		StringBuilder sb = new StringBuilder();
+		for (int i = len - 1; i >= 1; i--) {
+			int num = (int) chArr[i] - '0';
+			int nowNum = num + nextAdd;
+			nextAdd = nowNum >= 5 ? 1 : 0;
+			sb.append(0);
+		}
+
+		sb.insert(0, (int) (chArr[0] - '0') + nextAdd);
+		System.out.println(sb.toString());
+	}
+}
