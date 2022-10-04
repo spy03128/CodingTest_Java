d50e5484648bb4390d42413139d263860425bc3d
jihyeon <spy03128@naver.com>
2022-10-04 20:42:55 +0900

    BJ 1871 좋은 자동차 번호판

---
+++ src/BAEKJOON/B2_1871.java
@@ -0,0 +1,33 @@
+package BAEKJOON;
+
+import java.io.*;
+import java.util.*;
+
+
+
+public class B2_1871 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            String input = br.readLine();
+            String a = input.split("-")[0];
+            String b = input.split("-")[1];
+            int valueA = 0;
+            for(int j = 0; j < a.length(); j++){
+                int value = a.charAt(j) - 'A';
+                value *= Math.pow(26, 2 - j);
+                valueA += value;
+            }
+            int valueB = Integer.parseInt(b);
+            if(Math.abs(valueA - valueB) <= 100) bw.write("nice\n");
+            else bw.write("not nice\n");
+        }
+
+        bw.flush();
+    
+    }
+}
