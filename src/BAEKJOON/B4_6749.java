961a3f4c77bfaee2a1ea1e07634314cd2aaa6a3f
jihyeon <spy03128@naver.com>
2022-10-05 22:55:45 +0900

    BJ 6749 Next in line

---
+++ src/BAEKJOON/B4_6749.java
@@ -0,0 +1,14 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+public class B4_6749 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int differ = b - a;
+        System.out.println(b + differ);
+    }
+}
