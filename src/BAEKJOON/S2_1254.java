6d82730411735571baf814859af0bc46a5c0c544
jihyeon <spy03128@naver.com>
2022-10-17 23:08:54 +0900

    BJ 1254 팰린드롬 만들기

---
+++ src/BAEKJOON/S2_1254.java
@@ -0,0 +1,34 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S2_1254 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String s = in.readLine();
+		int ans = s.length();
+		for (int i = 0; i < s.length(); i++) {
+			if (isPalind(s.substring(i))) {
+				break;
+			}
+			ans++;
+		}
+		System.out.println(ans);
+		
+	}
+
+	private static boolean isPalind(String s) {
+		int start = 0;
+		int last = s.length() - 1;
+		while (start <= last) {
+			if (s.charAt(start) != s.charAt(last))
+				return false;
+			start++;
+			last--;
+		}
+		return true;
+	}
+}
